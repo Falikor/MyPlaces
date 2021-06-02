80c3c205307b02c314b4c96d19f832e46dd348e3
Falikor <Bugorv90@mail.ru>
2021-06-02 12:28:35 +0300

    added TavleViewControler + images

---
+++ MyPlaces/MainTableViewController.swift
@@ -0,0 +1,43 @@
+//
+//  MainTableViewController.swift
+//  MyPlaces
+//
+//  Created by Татьяна Татьяна on 02.06.2021.
+//
+
+import UIKit
+
+class MainTableViewController: UITableViewController {
+    let restaurantName = ["Балкан Гриль", "Бочка", "Вкусные истории", "Дастархан", "Индокитай", "Классик", "Шок", "Bonsai", "Burger Heroes", "Kitchen", "Love&Life", "Morris Pub", "Sherlock Holmes", "Speak Easy", "X.O"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurantName.count
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = restaurantName[indexPath.row]
+        cell.imageView?.image = UIImage(named: restaurantName[indexPath.row])
+        return cell
+    }
+
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
